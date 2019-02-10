db2bd3ad8b529808dfdb4846311da0797cebaae0
henry <henry.byrd@claricode.com>
2019-02-10 14:49:01 -0500

    Added project structure

---
+++ DijkstraDistances.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DijkstraDistances.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DijkstraDistances.Graphs/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DijkstraDistances.Graphs
+{
+    public class Class1
+    {
+    }
+}
